d343bac918e3aebde5aa69462f7caa5dd3011d0a
azaz7418 <wazaz7418@gmail.com>
2024-05-21 11:08:18 +0600

    redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,47 @@
-
-import './App.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './page/Home'
-import Archive from './page/Archive'
-import Category from './page/Category'
-import About from './page/About'
-import Contact from './page/Contact'
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./page/Home";
+import Archive from "./page/Archive";
+import Category from "./page/Category";
+import About from "./page/About";
+import Contact from "./page/Contact";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout/>,
-      children:[
+      element: <Layout />,
+      children: [
         {
-          path:"/",
-          element: <Home/>,
+          path: "/",
+          element: <Home />,
         },
         {
-          path:"/archive",
-          element: <Archive/>
+          path: "/archive",
+          element: <Archive />,
         },
         {
-          path:"/category",
-          element: <Category/>
+          path: "/category",
+          element: <Category />,
         },
         {
-          path:"/about",
-          element: <About/>
+          path: "/about",
+          element: <About />,
         },
         {
-          path:"/contact",
-          element: <Contact/>
+          path: "/contact",
+          element: <Contact />,
         },
-      ]
+      ],
     },
-  ])
+  ]);
 
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
